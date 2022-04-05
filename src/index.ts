c78fb1064ba1d207720c84ddb1f51b0383ae47f5
nattog <gpurssell@gmail.com>
2022-04-05 10:09:11 +0100

    Remove unnecessary useState

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 const useKey = (
   targetKey: string,
   onChange: (pressed: boolean, event: KeyboardEvent) => void
-): boolean => {
-  const [keyPressed, setKeyPressed] = useState<boolean>(false);
-
+) => {
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === targetKey) {
-        setKeyPressed(true);
         onChange(true, event);
       }
     };
 
     const handleKeyUp = (event: KeyboardEvent) => {
       if (event.key === targetKey) {
-        setKeyPressed(false);
         onChange(false, event);
       }
     };
@@ -29,8 +25,6 @@ const useKey = (
       window.removeEventListener("keyup", handleKeyUp);
     };
   }, [onChange, targetKey]);
-
-  return keyPressed;
 };
 
 export default useKey;
